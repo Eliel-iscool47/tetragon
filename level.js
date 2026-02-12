451c4e41386f5ff80abc8003f6f31ee0e6c48d29
Eliel-iscool47 <elieliscool47@yahoo.com>
2026-02-11 19:48:34 -0500

    ||UPDATE|| v1.01
    Added levels && power ups && a README file

--- level.js
+++ level.js
@@ -4,6 +4,10 @@ const level = {
     current: 1,
     new() {
         switch (true) {
+            case this.current <= 1:
+                mobs.spawn.default(utils.randomInt(collisions.border.left, collisions.border.right), utils.randomInt(collisions.border.top, collisions.border.bottom))
+                powerUps.gun.new(utils.randomInt(collisions.border.left, collisions.border.right), utils.randomInt(collisions.border.top, collisions.border.bottom))
+                break
             case this.current <= 5:
                 for (let index = 0; index < 5; index++) {
                     mobs.spawn.default(utils.randomInt(collisions.border.left, collisions.border.right), utils.randomInt(collisions.border.top, collisions.border.bottom))
@@ -18,12 +22,21 @@ const level = {
                 break
             case this.current <= 20:
                 for (let index = 0; index < 8; index++) {
-                    mobs.spawn.runner(utils.randomInt(collisions.border.left, collisions.border.right),utils.randomInt(collisions.border.top, collisions.border.bottom))
-                    mobs.spawn.tank(utils.randomInt(collisions.border.left, collisions.border.right),utils.randomInt(collisions.border.top, collisions.border.bottom))
+                    mobs.spawn.runner(utils.randomInt(collisions.border.left, collisions.border.right), utils.randomInt(collisions.border.top, collisions.border.bottom))
+                    mobs.spawn.tank(utils.randomInt(collisions.border.left, collisions.border.right), utils.randomInt(collisions.border.top, collisions.border.bottom))
                 }
                 break
             default:
+                for (let index = 0; index < 10; index++) {
+                    mobs.spawn.default(utils.randomInt(collisions.border.left, collisions.border.right), utils.randomInt(collisions.border.top, collisions.border.bottom))
+                    mobs.spawn.runner(utils.randomInt(collisions.border.left, collisions.border.right), utils.randomInt(collisions.border.top, collisions.border.bottom))
+                    mobs.spawn.tank(utils.randomInt(collisions.border.left, collisions.border.right), utils.randomInt(collisions.border.top, collisions.border.bottom))
+                }
                 break
+        }
+        for (let index = 0; index < 3; index++) {
+            powerUps.ammo.new(utils.randomInt(collisions.border.left, collisions.border.right), utils.randomInt(collisions.border.top, collisions.border.bottom))
+            powerUps.heal.new(utils.randomInt(collisions.border.left, collisions.border.right), utils.randomInt(collisions.border.top, collisions.border.bottom))
         }
     },
     next() {
